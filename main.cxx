45d80ec50e34739f0e5537c4133860426be1c3f8
number Zero <silverunicorn2011@yandex.ru>
2016-04-21 01:05:33 +0300

    Klein bottle looks nice =)

--- main.cxx
+++ main.cxx
@@ -4,19 +4,28 @@
 #include <SDL.h>
 #include <Eigen/Core>
 
-static long const grid_steps_u = 10;
-static long const grid_steps_v = 10;
-static long const sub_steps_u = 10;
-static long const sub_steps_v = 10;
+#ifdef NDEBUG
+static long const grid_steps_u = 32;
+static long const grid_steps_v = 24;
+static long const sub_steps_u = 8;
+static long const sub_steps_v = 8;
+#else
+static long const grid_steps_u = 24;
+static long const grid_steps_v = 16;
+static long const sub_steps_u = 3;
+static long const sub_steps_v = 3;
+#endif
 static long const steps_u = grid_steps_u * sub_steps_u;
 static long const steps_v = grid_steps_v * sub_steps_v;
-static double const min_u = -0.5 * M_PI;
+static double const min_u = -M_PI;
 static double const min_v = 0.0;
-static double const range_u = M_PI;
+static double const range_u = 2*M_PI;
 static double const range_v = M_PI;
 static double const step_u = range_u / steps_u;
 static double const step_v = range_v / steps_v;
 
+static double const grid_offset = 0.005;
+static double const surface_scale = 1 - grid_offset;
 // struct Point4
 // {
 // 	double x, y, z, w;
@@ -38,9 +47,17 @@ long pairs[6][2] = {
 	{ 1, 3 },
 	{ 2, 3 },
 };
-double speed[6] = { 0.9, 0.7, 0.0, 0.5, 0.0, 0.0 };
+double speed[6] = { 1./13., 1./11., 1./7., 1./5., 1./3., 1./2. };
+// double speed[6] = { 0.9, 0.7, 0.0, 0.5, 0.0, 0.0 };
 // double speed[6] = { 0.9, 0.8, 0.7, 0.6, 0.5, 0.4 };
 double angle[6] = { 0, 0, 0, 0, 0, 0 };
+double dt;
+
+bool animate = true;
+bool show_grid = true;
+bool show_surface = false;
+bool color_2d = true;
+bool color_3d = false;
 
 bool events()
 {
@@ -49,6 +66,28 @@ bool events()
 	{
 		switch(event.type)
 		{
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.scancode)
+				{
+					case SDL_SCANCODE_A:
+						animate = !animate;
+						break;
+					case SDL_SCANCODE_G:
+						show_grid = !show_grid;
+						break;
+					case SDL_SCANCODE_S:
+						show_surface = !show_surface;
+						break;
+					case SDL_SCANCODE_X:
+						color_2d = !color_2d;
+						break;
+					case SDL_SCANCODE_Z:
+						color_3d = !color_3d;
+						break;
+					case SDL_SCANCODE_ESCAPE:
+						return false;
+				}
+				break;
 			case SDL_QUIT:
 				return false;
 		}
@@ -59,10 +98,10 @@ bool events()
 Point4 makePoint(double u, double v)
 {
 	return {
-		sin(v) * cos(2*u),
-		sin(v) * sin(2*u),
-		cos(v),
-		0,
+		sin(u) * cos(v),
+		sin(u) * sin(v),
+		cos(u) + 0.33 * cos(2*v),
+		0.33 * sin(2*v),
 	};
 }
 
@@ -73,26 +112,26 @@ void drawPoint(Point4 const &pt, bool colorize = true)
 	glVertex3f(pt[0], pt[1], pt[2]);
 }
 
-void init()
+void colorize(long i, long j, bool colorize = false)
 {
-	for(long i = 0; i != steps_u; ++i)
-		for(long j = 0; j != steps_v; ++j)
-			grid[i][j] = makePoint(step_u * i, step_v * j);
+	if(!colorize)
+		return;
+	double u = step_u * i;
+	double v = step_v * j;
+	glColor4f(0.5 + 0.5 * cos(u), 0.5 * 0.5 + cos(2 * v), 0.0, 1.0);
 }
 
-void step()
+void init()
 {
-	long t_now = SDL_GetTicks();
-	double dt = (t_now - t_base) * 0.001;
-	t_base = t_now;
+	for(long j = 0; j != steps_v; ++j)
+		for(long i = 0; i != steps_u; ++i)
+			grid[j][i] = makePoint(min_u + step_u * i, min_v + step_v * j);
+}
 
-	glClearColor(0.0, 0.0, 0.2, 1.0);
-	glClearDepth(500.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	glScalef(150.0, 150.0, 150.0);
+Matrix4 mat;
 
-	Matrix4 mat;
+void calcRotationMatrix()
+{
 	mat.setIdentity();
 	for(long k = 0; k != 6; ++k)
 	{
@@ -106,39 +145,111 @@ void step()
 		rot(pairs[k][0], pairs[k][1]) = s;
 		rot(pairs[k][1], pairs[k][0]) = -s;
 		mat *= rot;
-		angle[k] += dt * speed[k];
+		if(animate)
+			angle[k] += dt * speed[k];
 	}
+}
 
+void drawGrid()
+{
+	glColor4f(0.0, 0.0, 0.8, 1.0);
 	for(long i = 0; i != steps_u; i += sub_steps_u)
 	{
-		glBegin(GL_LINE_LOOP);
+		glBegin(GL_LINE_STRIP);
 		for(long j = 0; j != steps_v; ++j)
-			drawPoint(mat * grid[j][i]);
+		{
+			colorize(i, j, color_2d);
+			drawPoint(mat * grid[j][i], color_3d);
+		}
+		long i2 = (steps_u - i) % steps_u;
+		colorize(i2, 0, color_2d);
+		drawPoint(mat * grid[0][i2], color_3d);
 		glEnd();
 	}
 	for(long j = 0; j != steps_v; j += sub_steps_v)
 	{
 		glBegin(GL_LINE_LOOP);
 		for(long i = 0; i != steps_u; ++i)
-			drawPoint(mat * grid[j][i]);
+		{
+			colorize(i, j, color_2d);
+			drawPoint(mat * grid[j][i], color_3d);
+		}
 		glEnd();
 	}
-/*
-	glColor4f(0.0, 1.0, 0.0, 0.3);
+}
+
+void drawSurface()
+{
+	mat *= surface_scale;
+	glColor4f(0.7, 0.7, 0.7, 0.5);
 	glBegin(GL_QUADS);
 	for(long j = 0; j != steps_v; ++j)
 	{
 		long j2 = (j + 1) % steps_v;
 		for(long i = 0; i != steps_u; ++i)
 		{
+			long i1 = i;
 			long i2 = (i + 1) % steps_u;
-			drawPoint(0.99 * mat * grid[j][i], false);
-			drawPoint(0.99 * mat * grid[j2][i], false);
-			drawPoint(0.99 * mat * grid[j2][i2], false);
-			drawPoint(0.99 * mat * grid[j][i2], false);
+			drawPoint(mat * grid[j][i1], false);
+			drawPoint(mat * grid[j][i2], false);
+			if(!j2)
+			{
+				i1 = (steps_u - i1) % steps_u;
+				i2 = (steps_u - i2) % steps_u;
+			}
+			drawPoint(mat * grid[j2][i2], false);
+			drawPoint(mat * grid[j2][i1], false);
 		}
 	}
-*/
+}
+
+void quad(long i, double r, double g, double b)
+{
+	static double const quad_size = 16;
+	glColor4f(r, g, b, 1.0);
+	glBegin(GL_QUADS);
+	glVertex2f(-400 + quad_size * 1.5*i, 300 - quad_size);
+	glVertex2f(-400 + quad_size * (1.5*i+1), 300 - quad_size);
+	glVertex2f(-400 + quad_size * (1.5*i+1), 300 - 2*quad_size);
+	glVertex2f(-400 + quad_size * 1.5*i, 300 - 2*quad_size);
+	glEnd();
+}
+
+void flag(long i, double r1, double g1, double b1, double r2, double g2, double b2, bool value)
+{
+	if(value)
+		quad(i, r1, g1, b1);
+	else
+		quad(i, r2, g2, b2);
+}
+
+void step()
+{
+	long t_now = SDL_GetTicks();
+	dt = (t_now - t_base) * 0.001;
+	t_base = t_now;
+
+	glClearColor(0.0, 0.0, 0.2, 1.0);
+	glClearDepth(500.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	flag(1, 0.0, 1.0, 0.0, 0.8, 0.0, 0.0, animate);
+	flag(2, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, show_grid);
+	flag(3, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, show_surface);
+	flag(4, 0.3, 0.8, 0.0, 0.6, 0.6, 0.6, color_2d);
+	flag(5, 1.0, 0.3, 0.5, 0.6, 0.6, 0.6, color_3d);
+
+	glScalef(150.0, 150.0, 150.0);
+
+	calcRotationMatrix();
+
+	if(show_grid)
+		drawGrid();
+
+	if(show_surface)
+		drawSurface();
+
 	glEnd();
 	glFlush();
 	glFinish();
@@ -154,7 +265,7 @@ void run()
 void initSDL()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("Klein bottle test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+	window = SDL_CreateWindow("Klein bottle test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768, SDL_WINDOW_OPENGL);
 	context = SDL_GL_CreateContext(window);
 	SDL_GL_MakeCurrent(window, context);
 	t_base = SDL_GetTicks();
@@ -163,12 +274,12 @@ void initSDL()
 void initGL()
 {
 	glDisable(GL_LIGHTING);
-	glEnable(GL_DEPTH);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH);
 
-	glEnable(GL_POLYGON_SMOOTH);
-	glEnable(GL_LINE_SMOOTH);
-	glLineWidth(2.5);
+// 	glEnable(GL_POLYGON_SMOOTH);
+// 	glEnable(GL_LINE_SMOOTH);
+	glLineWidth(0.7);
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
